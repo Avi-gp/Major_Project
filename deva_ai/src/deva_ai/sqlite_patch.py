9f3e80b82e10ded5a6eb82a422f4648773559e88
Gupta Suryansh <135865433+Avi-gp@users.noreply.github.com>
2025-11-13 06:09:05 +0000

    solve bug

--- deva_ai/src/deva_ai/sqlite_patch.py
+++ deva_ai/src/deva_ai/sqlite_patch.py
@@ -1,6 +1,10 @@
 # sqlite_patch.py
 import sys
 import pysqlite3
+import warnings
+
+# Suppress pydantic deprecation warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="pydantic")
 
 # Force Python to use pysqlite3 as sqlite3
 sys.modules["sqlite3"] = pysqlite3
